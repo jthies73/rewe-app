fac296128d138a5385dd1582d0224184c9e03434
jthies <johannes.thies.73@gmail.com>
2023-08-20 00:21:46 +0200

    Add Github Actions Scrips

--- rewe-fe/src/components/ExploreContainer.tsx
+++ rewe-fe/src/components/ExploreContainer.tsx
@@ -2,16 +2,26 @@ import React from 'react';
 import './ExploreContainer.css';
 
 interface ContainerProps {
-  name: string;
+    name: string;
 }
 
-const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
-  return (
-    <div className="container">
-      <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
-  );
+const ExploreContainer: React.FC<ContainerProps> = ({name}) => {
+    return (
+        <div className="container">
+            <strong>{name}</strong>
+            <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI
+                Components</a></p>
+            <button onClick={() => {
+                navigator.mediaDevices.getUserMedia({
+                    video: {
+                        facingMode: "user"
+                    }
+
+                })
+            }}>Show my face
+            </button>
+        </div>
+    );
 };
 
 export default ExploreContainer;
