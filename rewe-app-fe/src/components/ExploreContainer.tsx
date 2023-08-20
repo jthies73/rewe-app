5c856a82169c0f14538710929bd0fc8fb739c23f
jthies <johannes.thies.73@gmail.com>
2023-08-20 03:42:13 +0200

    Add prettier

--- rewe-app-fe/src/components/ExploreContainer.tsx
+++ rewe-app-fe/src/components/ExploreContainer.tsx
@@ -1,27 +1,30 @@
-import React from 'react';
-import './ExploreContainer.css';
+import { IonButton } from "@ionic/react";
+import React from "react";
+
+import "./ExploreContainer.css";
 
 interface ContainerProps {
-    name: string;
+	name: string;
 }
 
-const ExploreContainer: React.FC<ContainerProps> = ({name}) => {
-    return (
-        <div className="container">
-            <strong>{name}</strong>
-            <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI
-                Components</a></p>
-            <button onClick={() => {
-                navigator.mediaDevices.getUserMedia({
-                    video: {
-                        facingMode: "user"
-                    }
-
-                })
-            }}>Show my face
-            </button>
-        </div>
-    );
+const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+	return (
+		<div className="container">
+			<strong>{name.toUpperCase()}</strong>
+			<br />
+			<IonButton
+				onClick={() => {
+					navigator.mediaDevices.getUserMedia({
+						video: {
+							facingMode: "user",
+						},
+					});
+				}}
+			>
+				Show my face
+			</IonButton>
+		</div>
+	);
 };
 
 export default ExploreContainer;
