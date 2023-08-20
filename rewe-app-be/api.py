d90ac503c24a2990a3890ddb397953f846beb7d3
Christian Vielhaus <christian.vielhaus@tu-dresden.de>
2023-08-20 21:54:46 +0200

    Create image upload endpoint in backend

--- rewe-app-be/api.py
+++ rewe-app-be/api.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
 
 app = FastAPI()
 
@@ -8,6 +8,19 @@ app = FastAPI()
 @app.get("/api/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/api/images")
+def process_upload_image(image: UploadFile):
+    try:
+        contents = image.file.read()
+        # TODO: Process file from here
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        image.file.close()
+
+    return {"message": f"Successfully uploaded {image.filename}"}
 
 
 @app.get("/api/items/{item_id}")
