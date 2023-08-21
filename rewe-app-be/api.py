75eba50beb82871e3188a9e628acaeb5df03dec3
jthies <johannes.thies.73@gmail.com>
2023-08-21 03:34:05 +0200

    just return filename

--- rewe-app-be/api.py
+++ rewe-app-be/api.py
@@ -12,13 +12,13 @@ def read_root():
 
 @app.post("/api/images")
 async def process_upload_image(image: UploadFile):
-    try:
-        contents = await image.file.read()
-        # TODO: Process file from here
-    except Exception:
-        return {"message": "There was an error uploading the file"}
-    finally:
-       await image.file.close()
+#     try:
+#         contents = await image.file.read()
+#         # TODO: Process file from here
+#     except Exception:
+#         return {"message": "There was an error uploading the file"}
+#     finally:
+#        await image.file.close()
 
     return {"message": f"Successfully uploaded {image.filename}"}
 
