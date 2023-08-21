cc6038a7ca9020c3321dbb3e5ee6f6611b9af693
jthies <johannes.thies.73@gmail.com>
2023-08-21 04:06:58 +0200

    Use floating action button for taking photos

--- rewe-app-fe/src/pages/Page.tsx
+++ rewe-app-fe/src/pages/Page.tsx
@@ -1,19 +1,42 @@
 import {
+	Camera,
+	CameraDirection,
+	CameraResultType,
+	CameraSource,
+	Photo,
+} from "@capacitor/camera";
+import {
 	IonButtons,
 	IonContent,
+	IonFab,
+	IonFabButton,
 	IonHeader,
+	IonIcon,
 	IonMenuButton,
 	IonPage,
 	IonTitle,
 	IonToolbar,
 } from "@ionic/react";
-import React from "react";
+import { camera } from "ionicons/icons";
+import React, { useState } from "react";
 import { useParams } from "react-router";
 
 import ExploreContainer from "../components/ExploreContainer";
 import "./Page.css";
 
+const takePhoto = async (direction: "rear" | "front") => {
+	return await Camera.getPhoto({
+		quality: 100,
+		allowEditing: true,
+		resultType: CameraResultType.Uri,
+		source: CameraSource.Camera,
+		direction:
+			direction === "rear" ? CameraDirection.Rear : CameraDirection.Front,
+	});
+};
+
 const Page: React.FC = () => {
+	const [photos, setPhotos] = useState<Photo[]>([]);
 	const { name } = useParams<{ name: string }>();
 
 	return (
@@ -33,8 +56,21 @@ const Page: React.FC = () => {
 						<IonTitle size="large">{name + "TEST"}</IonTitle>
 					</IonToolbar>
 				</IonHeader>
-				<ExploreContainer name={name} />
+				<ExploreContainer photos={photos} name={name} />
 			</IonContent>
+
+			{/* Add the fab button with the camera icon */}
+			<IonFab vertical={"bottom"} horizontal={"end"}>
+				<IonFabButton
+					onClick={async () => {
+						const photo = await takePhoto("front");
+						if (!photo) return;
+						setPhotos((photos) => [...photos, photo]);
+					}}
+				>
+					<IonIcon icon={camera} />
+				</IonFabButton>
+			</IonFab>
 		</IonPage>
 	);
 };
