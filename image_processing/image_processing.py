591ace9d16b0679aab99d2b69effdeefda242fad
100daggers <dinesh.soudagar@gmail.com>
2023-08-25 17:11:46 +0200

    Baseline working image recognition

--- image_processing/image_processing.py
+++ image_processing/image_processing.py
@@ -6,11 +6,9 @@ import cv2
 from matplotlib import pyplot as plt
 import numpy as np
 
-txt_image = "random.jpg"
+txt_image = "decathlon_bill.jpg"
 
 reader = easyocr.Reader(["en"], gpu=True)
-text_output = reader.readtext(txt_image)  # , paragraph=True)
+text_output = reader.readtext(txt_image, paragraph=True)
 
 print(text_output)
-for text_data in text_output:
-    print(text_data[1])
