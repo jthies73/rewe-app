324b6f236b43175f0ac19ebe97789828c2f709b6
100daggers <dinesh.soudagar@gmail.com>
2023-08-25 17:41:40 +0200

    Baseline working OCR

--- image_processing/image_processing.py
+++ image_processing/image_processing.py
@@ -3,7 +3,7 @@ This module extracts the text from the images
 """
 import easyocr
 
-txt_image = "decathlon_bill.jpg"
+txt_image = "test_images/rewe_dresden_bill.jpg"
 
 reader = easyocr.Reader(["en", "de"], gpu=True)
 text_output = reader.readtext(txt_image, paragraph=True)
