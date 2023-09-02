95ba35097e1c2b236f6a343101a8574d1d9fa76e
jthies <johannes.thies.73@gmail.com>
2023-09-02 03:27:17 +0200

    Display api results

---
+++ rewe-app-fe/src/model/expense.ts
@@ -0,0 +1,11 @@
+export interface Expense {
+	expense_id: number;
+	bill_id: number;
+	user_id: number;
+	name: string;
+	value: number;
+	price_per_item: number;
+	weight: number;
+	price_per_kg: number;
+	tags: string;
+}
