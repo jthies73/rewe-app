10da40695c76d025951f88b26c0d5d9d481ab2e0
Johannes Thies <johannes.thies.73@gmail.com>
2023-10-28 02:03:07 +0200

    change placeholder name

--- rewe-app-fe/src/pages/LoginPage.tsx
+++ rewe-app-fe/src/pages/LoginPage.tsx
@@ -78,7 +78,7 @@ const LoginPage: React.FC = () => {
 										name="username"
 										type="username"
 										autoComplete="username"
-										placeholder="Harry"
+										placeholder="Rainer"
 										// required
 										className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 p-3"
 										onChange={handleUsernameChange}
@@ -101,7 +101,7 @@ const LoginPage: React.FC = () => {
 										name="password"
 										type="password"
 										autoComplete="current-password"
-										placeholder="Bawls"
+										placeholder="Zufall"
 										// required
 										className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 p-3"
 										onChange={handlePasswordChange}
