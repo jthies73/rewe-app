ce054f3556915b3dfe772fbfc2607affc2e64118
jthies73 <34398694+jthies73@users.noreply.github.com>
2023-10-28 02:00:38 +0200

    refactor: Refactor Overview Page (#47)
    * enable linting and testing
    
    * use jest for testing
    
    * upgrade node
    
    * combine fab buttons in fab list
    
    * add ebons to git
    
    * change github actions checkout to node v3

--- rewe-app-fe/src/utils/api.test.ts
+++ rewe-app-fe/src/utils/api.test.ts
@@ -1,7 +1,8 @@
-import React from "react";
+import { generateDateFileName } from "./stringUtils";
 
 describe("Sample Test File", () => {
 	it("Should return Hello World", () => {
-		expect("Hello World").toEqual("Hello World");
+		const name = generateDateFileName(new Date());
+		expect(name).not.toBe(null);
 	});
 });
