36148aedffec09c1712ac336f42ab129419586fa
Johannes Thies <johannes.thies.73@gmail.com>
2023-10-30 23:37:08 +0100

    whitespace commmit

--- rewe-app-fe/src/zustand/authStore.ts
+++ rewe-app-fe/src/zustand/authStore.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
-// Define the store state and actions
+// Define the store state  and actions
 interface AuthStore {
 	token: string;
 	setToken: (token: string) => void;
