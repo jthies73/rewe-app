9a6e3e200406fbd1cd25242857c7d3bfffd311ae
jthies73 <johannes.thies.73@gmail.com>
2024-06-15 16:18:00 +0200

    fix charts

--- rewe-app-fe/src/zustand/chartDataStore.ts
+++ rewe-app-fe/src/zustand/chartDataStore.ts
@@ -3,11 +3,11 @@ import { create } from "zustand";
 // Define the store state and actions
 interface ChartDataStore {
 	daily: { day: string; value: number }[];
-	setDaily: (daily: { day: string; value: number }[]) => void;
+	setDaily: (daily: { x: string; y: number }[]) => void;
 	monthly: { month: string; value: number }[];
-	setMonthly: (monthly: { month: string; value: number }[]) => void;
+	setMonthly: (monthly: { x: string; y: number }[]) => void;
 	yearly: { year: string; value: number }[];
-	setYearly: (yearly: { year: string; value: number }[]) => void;
+	setYearly: (yearly: { x: string; y: number }[]) => void;
 }
 
 // Create the Zustand store
@@ -15,23 +15,33 @@ const useChartDataStore = create<ChartDataStore>((set) => ({
 	daily: [],
 	monthly: [],
 	yearly: [],
-	setDaily: (daily: { day: string; value: number }[]) =>
+	setDaily: (daily) =>
 		set(() => ({
 			daily: daily.map((d) => ({
-				day: new Date(d.day).toLocaleDateString("en-US", {
+				day: new Date(d.x).toLocaleDateString("en-US", {
 					month: "short", // "Nov"
 					day: "2-digit", // "05"
 				}),
-				value: d.value,
+				value: d.y,
 			})),
 		})),
 	setMonthly: (monthly) =>
 		set(() => ({
-			monthly: monthly,
+			monthly: monthly.map((m) => ({
+				month: new Date(m.x).toLocaleDateString("en-US", {
+					month: "short", // "Nov"
+				}),
+				value: m.y,
+			})),
 		})),
 	setYearly: (yearly) =>
 		set(() => ({
-			yearly: yearly,
+			yearly: yearly.map((y) => ({
+				year: new Date(y.x).toLocaleDateString("en-US", {
+					year: "numeric", // "2021"
+				}),
+				value: y.y,
+			})),
 		})),
 }));
 
